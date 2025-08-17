6d752f780a4b19393b8e80d8983727da0c629ab0
Vaibhav <vaibhavverma9595@gmail.com>
2025-08-17 22:24:18 +0530

    Some changes

---
+++ models/userDataModel.js
@@ -0,0 +1,31 @@
+// import mongoose from "mongoose";
+// const userDataSchema = new mongoose.Schema({
+//     firstName:{
+//         type:String,
+//         required:true,
+//         default:""
+//     }, 
+//     email: {
+//         type: String,
+//         unique: true,
+//     },
+//     lastName:{
+//         type:String,
+//         default:""
+//     },
+//    createdAt:{
+//         type:Date,
+//         default:Date.now
+//    },
+//     profession:{
+//         type:String,
+//         default:"",
+//     },
+//     profileImage:{
+//         type:String,
+//         default:"data:https://res.cloudinary.com/dub24ezgr/image/upload/v1754241821/upload_img_i6omii.jpg"
+//     }
+// })
+
+// const userDatamodelvar =mongoose.model('UserData', userDataSchema, 'userdatas');
+// export default userDatamodelvar;
